46865b890c1d820ad4d9a0ed116bf57991f62a79
Daniel <danielnkurunziza@gmail.com>
2025-10-09 22:05:12 +0100

    Add project files.

---
+++ TowerOfHanoi/HanoiTower.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TowerOfHanoi;
+
+public class HanoiTower
+{
+    public int DiscsCount { get; private set; }
+    public int MovesCount { get; private set; }
+    public Stack<int> From { get; private set; }
+    public Stack<int> To { get; private set; }
+    public Stack<int> Auxillary { get; private set; }
+    public event EventHandler<EventArgs> MoveCompleted;
+
+    public HanoiTower(int discs)
+    {
+        DiscsCount = discs;
+        From = new Stack<int>();
+        To = new Stack<int>();
+        Auxillary = new Stack<int>();
+        for (int i = 1; i < discs; i++)
+        {
+            int size = discs - i + 1;
+            From.Push(size);
+        }
+
+    }
+
+    public void Start()
+    {
+        Move(DiscsCount, From, To, Auxillary);
+    }
+
+    public void Move(int discs, Stack<int> from, Stack<int> to, Stack<int> auxillary)
+    {
+        if (discs > 0)
+        {
+            Move(discs - 1, from, auxillary, to);
+
+            to.Push(from.Pop());
+            MovesCount++;
+            MoveCompleted?.Invoke(this, EventArgs.Empty);
+
+            Move(discs - 1, auxillary, to, from);
+        }
+    }
+}
---
+++ TowerOfHanoi/Program.cs
@@ -0,0 +1,95 @@
+﻿namespace TowerOfHanoi;
+
+using System;
+using System.Collections.Generic;
+
+/// <summary>
+/// Following Marcin Jamro's tutorial on Tower of Hanoi using stacks.
+/// </summary>
+class Program
+{
+    private const int DISCS_COUNT = 10;
+    private const int DELAY_MS = 50;
+    private static int _columnSize = 30;
+    static void Main(string[] args)
+    {
+        _columnSize = Math.Max(6, GetDiscWidth(DISCS_COUNT) + 2);
+        HanoiTower algorithm = new HanoiTower(DISCS_COUNT);
+        algorithm.MoveCompleted += Algorithm_Visualize;
+        Algorithm_Visualize(algorithm, EventArgs.Empty);
+        algorithm.Start();
+    }
+
+    private static void Algorithm_Visualize(object? sender, EventArgs e)
+    {
+        Console.Clear();
+
+        HanoiTower algorithm = (HanoiTower)sender;
+        if (algorithm.DiscsCount <= 0) { return; }
+
+        char[][] visualisation = InitializeVisualization(algorithm);
+        PrepareColumn(visualisation, 1, algorithm.DiscsCount, algorithm.From);
+        PrepareColumn(visualisation, 2, algorithm.DiscsCount, algorithm.To);
+        PrepareColumn(visualisation, 3, algorithm.DiscsCount, algorithm.Auxillary);
+
+        Console.WriteLine(Center("FROM") + Center("TO") + Center("AUXILLARY"));
+        DrawVisualization(visualisation);
+        Console.WriteLine();
+        Console.WriteLine($"Number of moves: {algorithm.MovesCount}");
+        Console.WriteLine($"Number of discs: {algorithm.DiscsCount}");
+
+        Thread.Sleep(DELAY_MS);
+    }
+
+    private static string? Center(string text)
+    {
+        int margin = (_columnSize - text.Length) / 2;
+        return text.PadLeft(margin + text.Length).PadRight(_columnSize);
+    }
+
+    private static void DrawVisualization(char[][] visualisation)
+    {
+        for (int y = 0; y < visualisation.Length; y++)
+        {
+            Console.WriteLine(visualisation[y]);
+        }
+    }
+
+    private static void PrepareColumn(char[][] visualisation, int column, int discsCount, Stack<int> stack)
+    {
+        int margin = _columnSize * (column - 1);
+
+        for (int y = 0; y < stack.Count; y++)
+        {
+            int size = stack.ElementAt(y);
+            int row = discsCount - (stack.Count - y);
+            int columnStart = margin + discsCount - size;
+            int columnEnd = columnStart + GetDiscWidth(size);
+
+            for (int x = columnStart; x < columnEnd; x++)
+            {
+                visualisation[row][x] = '=';
+            }
+        }
+    }
+
+    private static int GetDiscWidth(int size)
+    {
+        return size * 2 - 1;
+    }
+
+    private static char[][] InitializeVisualization(HanoiTower algorithm)
+    {
+        char[][] visualisation = new char[algorithm.DiscsCount][];
+
+        for (int y = 0; y < visualisation.Length; y++)
+        {
+            visualisation[y] = new char[_columnSize * 3];
+            for (int x = 0; x < _columnSize * 3; x++)
+            {
+                visualisation[y][x] = ' ';
+            }
+        }
+        return visualisation;
+    }
+}
