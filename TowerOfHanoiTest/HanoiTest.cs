c75f47b6edf7a540e63ddd47819a049020c4b36f
Daniel <danielnkurunziza@gmail.com>
2025-11-11 15:00:20 +0000

    test: add ms test project for validation testing

---
+++ TowerOfHanoiTest/HanoiTest.cs
@@ -0,0 +1,15 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TowerOfHanoi;
+
+namespace TowerOfHanoiTest;
+
+[TestClass]
+public class HanoiTest
+{
+    [TestMethod]
+    public void CheckIfPrints()
+    {
+        HanoiTower tower = new HanoiTower(5);
+        
+    }
+}
